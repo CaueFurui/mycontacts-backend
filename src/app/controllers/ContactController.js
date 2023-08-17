84cc40165d6a92de9880ee4dd63cdc872a0b25d6
cauefurui <cauefurui@gmail.com>
2023-08-17 14:00:04 -0300

    adding update contact

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -34,7 +34,7 @@ class ContactController {
     const contactExists = await ContactsRepository.findByEmail(email);
 
     if (contactExists) {
-      return response.status(400).json({ error: 'This email is already been taken' });
+      return response.status(400).json({ error: 'This email is already in use' });
     }
 
     const contact = await ContactsRepository.create({
@@ -44,8 +44,35 @@ class ContactController {
     response.json(contact);
   }
 
-  update(request, response) {
+  async update(request, response) {
     // Editar um registro
+    const { id } = request.params;
+
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This email is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
